de895a363e4005c1d362a48f21e4426c303e75c5
Charukajalanidu-ofc <154059188+Charukajalanidu-ofc@users.noreply.github.com>
2024-11-09 19:14:41 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ module.exports = {
   OWNER: "94775228949",//Enter Owner Number
   USER_NAME: "mr charuka",//Enter Your UserName 
   PASSWORD: "12345678",
-  HIRU_GROUP_JID: ["120363307730093301@g.us"],
+  HIRU_GROUP_JID: ["120363339224309117@g.us"],
 };
