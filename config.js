8ff9bdb099de916e3c689093c4bafd598ecd887a
Charuka1 <129839125+Charuka1@users.noreply.github.com>
2024-11-10 11:16:57 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 
 
 module.exports = {
-  PREFIX: ".", //set Prefix
-  OWNER: "94775228949",//Enter Owner Number
-  USER_NAME: "mr charuka",//Enter Your UserName 
-  PASSWORD: "12345678",
-  HIRU_GROUP_JID: ["120363339224309117@g.us"]
+  PREFIX: process.env.PREFIX || '.',
+  OWNER: process.env.OWNER || '94775228949', //Enter Owner Number
+  USER_NAME: process.env.USER_NAME || 'r charuka', //Enter Your UserName 
+  PASSWORD: process.env.PASSWORD || '12345678',
+  HIRU_GROUP_JID:process.env.HIRU_GROUP_JID || '120363339224309117@g.us',
 };
